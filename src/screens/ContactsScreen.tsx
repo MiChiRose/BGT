6808815376ded249278cdc6807e6c6df286f467d
Yura Menschikov <yura.menschikov@icloud.com>
2023-02-14 01:38:00 +0300

    Small changes

--- src/screens/ContactsScreen.tsx
+++ src/screens/ContactsScreen.tsx
@@ -1,14 +1,18 @@
 import React, {memo} from "react";
-import {Image, ScrollView, Text, View, StyleSheet} from "react-native";
-import {Container} from "../components/Container";
+import {Image, ScrollView, Text, View, StyleSheet, ImageProps} from "react-native";
+import Container from "../components/Container";
 import {MapViewBGTFixed} from "../components/MapViewBGTFixed";
 import {contactsData_Phones, contactsData_Address} from "../constants/data";
 
-const MenuItem = ({data, image}) => {
+interface MenuItemProps {
+    data: Array<any>;
+    image: any;
+}
+const MenuItem = ({data, image}: MenuItemProps): JSX.Element => {
     return (
         <View style={styles.menuItemContainer}>
             {data.map((element, index) => (
-                <View style={{marginBottom: 10, flexDirection: 'row'}} key={index}>
+                <View style={styles.dataContainer} key={index}>
                     <View>
                         {index === 0 ? <Image
                             source={image}
@@ -16,8 +20,8 @@ const MenuItem = ({data, image}) => {
                         /> : <View style={styles.image}/>}
                     </View>
                     <View style={{flex: 1}}>
-                        <Text>{element.header}</Text>
-                        <Text>{element.title}</Text>
+                        <Text style={styles.textHeader}>{element.header}</Text>
+                        <Text style={styles.textTitle}>{element.title}</Text>
                     </View>
                 </View>
             ))}
@@ -28,7 +32,7 @@ const MenuItem = ({data, image}) => {
 const ContactsScreen = (): JSX.Element => {
     return (
         <Container>
-            <ScrollView style={{flex: 1}}>
+            <ScrollView style={styles.flex}>
                 <MapViewBGTFixed/>
                 <MenuItem
                     data={contactsData_Address}
@@ -56,5 +60,20 @@ const styles = StyleSheet.create({
         marginRight: 115,
         marginTop: 20,
         flexDirection: 'column'
+    },
+    dataContainer: {
+        marginBottom: 10,
+        flexDirection: 'row'
+    },
+    textHeader: {
+        fontSize: 14,
+        fontWeight: "700"
+    },
+    textTitle: {
+        fontSize: 14,
+        fontWeight: "400"
+    },
+    flex: {
+        flex: 1
     }
 })
