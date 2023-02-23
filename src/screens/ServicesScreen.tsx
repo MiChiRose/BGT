2326f67e8d14dad685cb962bb50239b78fde0844
Yura Menschikov <yura.menschikov@icloud.com>
2023-02-23 12:37:33 +0300

    Add new screen

---
+++ src/screens/ServicesScreen.tsx
@@ -0,0 +1,12 @@
+import React, {memo} from "react";
+import {Button, View} from "react-native";
+
+const ServicesScreen = ({ }) => {
+    return (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+
+        </View>
+    );
+}
+
+export default memo(ServicesScreen)
