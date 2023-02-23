7cfb52974a67a12c0850ab43a80f2ff583d08f1b
Yura Menschikov <yura.menschikov@icloud.com>
2023-02-23 14:52:42 +0300

    Add components

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,44 @@
 import React, {memo} from "react";
-import {Button, View} from "react-native";
+import {ScrollView} from "react-native";
+import GradientButton from "../components/GradientButton";
+import {CarouselHome} from "./components/homeScreen/Carousel";
 
-const HomeScreen = ({  }) => {
+const buttons = [
+    {
+        title: "Арматура",
+        onPress: () => {
+        }
+    },
+    {
+        title: "Приборы и устройства",
+        onPress: () => {
+        }
+    },
+    {
+        title: "Оборудование для систем газоснабжения",
+        onPress: () => {
+        }
+    },
+    {
+        title: "Технологическое оборудование для ГНС и АЗС",
+        onPress: () => {
+        }
+    }
+]
+
+const HomeScreen = ({}) => {
     return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-
-        </View>
+        <ScrollView style={{flex: 1, paddingHorizontal: 25, paddingTop: 25}}>
+            <CarouselHome/>
+            {buttons.map((item, index) => (
+                <GradientButton
+                    key={index}
+                    text={item.title}
+                    onPress={item.onPress}
+                    style={{marginBottom: 30}}
+                />
+            ))}
+        </ScrollView>
     );
 }
 
