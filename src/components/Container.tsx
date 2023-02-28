8b8d68d6abe71cb3d6df4d41b78f0955408ed44c
Yura Menschikov <yura.menschikov@icloud.com>
2023-02-28 23:00:36 +0300

    Small changes

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,15 +1,20 @@
-import {SafeAreaView} from "react-native";
 import React, {memo} from "react";
+import {SafeAreaView, StyleSheet} from "react-native";
 
 interface Props {
     children: any;
 }
+
 const Container = ({children}: Props): JSX.Element => {
     return (
-        <SafeAreaView style={{flex: 1}}>
+        <SafeAreaView style={styles.container}>
             {children}
         </SafeAreaView>
     )
 }
 
 export default memo(Container);
+
+const styles = StyleSheet.create({
+    container: {flex: 1, marginHorizontal: 25, marginVertical: 30}
+})
