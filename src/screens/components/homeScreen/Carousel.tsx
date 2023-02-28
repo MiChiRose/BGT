7481b1512ac5c3e884a525a28e3f144ad9796e74
Yura Menschikov <yura.menschikov@icloud.com>
2023-02-28 23:00:58 +0300

    Change data

--- src/screens/components/homeScreen/Carousel.tsx
+++ src/screens/components/homeScreen/Carousel.tsx
@@ -1,77 +1,70 @@
 import React from "react";
-import {Dimensions, Text, View} from "react-native";
+import {View, Image} from "react-native";
 import Carousel from 'react-native-reanimated-carousel';
 import {useSharedValue} from "react-native-reanimated";
 import {PaginationItem} from "./PaginationItem";
-
-const width = Dimensions.get('screen').width
-
-const colors = [
-    "#26292E",
-    "#899F9C",
-    "#B3C680",
-    "#5C6265",
-    "#F5D399",
-    "#F1F1F1",
-];
+import {width} from "../../../constants/deviceParam";
+import {carouselSlots} from "../../../constants/data";
 export const CarouselHome = (): JSX.Element => {
     const progressValue = useSharedValue<number>(0)
     return (
         <View style={{marginBottom: 30, borderRadius: 15}}>
-        <Carousel
-            loop
-            pagingEnabled={true}
-            snapEnabled={true}
-            autoPlay={true}
-            autoPlayInterval={5000}
-            width={width - 50}
-            height={(width - 50) / 2}
-            mode="parallax"
-            modeConfig={{
-                parallaxScrollingScale: 0.9,
-                parallaxScrollingOffset: 50,
-            }}
-            onProgressChange={(_, absoluteProgress) =>
-                (progressValue.value = absoluteProgress)
-            }
-            data={colors}
-            scrollAnimationDuration={1000}
-            // onSnapToItem={(index:number) => console.log('current index:', index)}
-            renderItem={({ index }:{index:number}) => (
-                <View
-                    style={{
-                        backgroundColor: 'red',
-                        flex: 1,
-                        justifyContent: 'center',
-                    }}
-                >
-                    <Text style={{ textAlign: 'center', fontSize: 30 }}>
-                        {index}
-                    </Text>
-                </View>
-            )}
-        />
+            <Carousel
+                loop
+                pagingEnabled={true}
+                snapEnabled={true}
+                autoPlay={true}
+                autoPlayInterval={5000}
+                width={width - 50}
+                height={(width - 50) / 1.5}
+                mode="parallax"
+                modeConfig={{
+                    parallaxScrollingScale: 0.9,
+                    parallaxScrollingOffset: 50,
+                }}
+                onProgressChange={(_, absoluteProgress) =>
+                    (progressValue.value = absoluteProgress)
+                }
+                data={carouselSlots}
+                scrollAnimationDuration={1000}
+                renderItem={({item, index}: { item: string, index: number }) => (
+                    <View
+                        style={{
+                            backgroundColor: 'red',
+                            flex: 1,
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Image
+                            style={{width: '100%', flex: 1, backgroundColor: 'blue'}}
+                            //@ts-ignore
+                            source={item}
+                            resizeMode={'cover'}
+                        />
+                    </View>
+                )}
+            />
             <View
                 style={{
-                            flexDirection: "row",
-                            justifyContent: "space-between",
-                            width: 100,
-                            alignSelf: "center",
-                        }
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    width: 50,
+                    alignSelf: "center",
+                }
                 }
             >
-                {colors.map((backgroundColor, index) => {
+                {carouselSlots.map((item, index) => {
                     return (
                         <PaginationItem
-                            backgroundColor={backgroundColor}
+                            backgroundColor={"black"}
                             animValue={progressValue}
                             index={index}
                             key={index}
-                            length={colors.length}
+                            length={carouselSlots.length}
                         />
                     );
                 })}
             </View>
-            </View>
+        </View>
     )
 }
