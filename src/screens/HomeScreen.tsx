a41d9e5f55210eeea3ab54bc9e85e49fd0a8554a
C5345425 <971656316607f16c7e45d3e64a7cfcabf047cc96>
2023-03-17 01:35:15 +0300

    Fill data, small changes

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,69 +1,66 @@
 import React, {memo, useState} from "react";
-import {ScrollView, TouchableOpacity, Text, Image, View} from "react-native";
+import {Text, View} from "react-native";
 import GradientButton from "../components/GradientButton";
 import {CarouselHome} from "./components/homeScreen/Carousel";
-import {Color} from "../constants/color";
-import {aboutCompanyText} from "../constants/data";
+import {aboutCompanyText, catalogData} from "../constants/data";
 import Container from "../components/Container";
 import DropdownItem from "./components/homeScreen/DropdownItem";
+import {CustomScrollView} from "../components/CustomScrollView";
+import {ServicesInfo} from "./components/homeScreen/ServicesInfo";
 
-const buttons = [
-    {
-        title: "Арматура",
-        onPress: () => {
-        }
-    },
-    {
-        title: "Приборы и устройства",
-        onPress: () => {
-        }
-    },
-    {
-        title: "Оборудование для систем газоснабжения",
-        onPress: () => {
-        }
-    },
-    {
-        title: "Технологическое оборудование для ГНС и АЗС",
-        onPress: () => {
-        }
-    }
-];
+type Props = {
+    navigation: any
+}
 
-const HomeScreen = ({}) => {
+const HomeScreen = ({navigation: {navigate}}: Props) => {
     const [showServices, setShowServices] = useState<boolean>(false);
     const [showAbout, setShowAbout] = useState<boolean>(false);
     const [showNews, setShowNews] = useState<boolean>(false);
 
+    const buttons = [
+        {
+            title: "Арматура",
+            onPress: () => navigate("CatalogDetailsHome", {data: catalogData[3].data, name: catalogData[3].title})
+        },
+        {
+            title: "Приборы и устройства",
+            onPress: () => navigate("CatalogDetailsHome", {data: catalogData[0].data, name: catalogData[0].title})
+        },
+        {
+            title: "Оборудование для систем газоснабжения",
+            onPress: () => navigate("CatalogDetailsHome", {data: catalogData[1].data, name: catalogData[1].title})
+        },
+        {
+            title: "Технологическое оборудование для ГНС и АЗС",
+            onPress: () => navigate("CatalogDetailsHome", {data: catalogData[2].data, name: catalogData[2].title})
+        }
+    ];
+
     return (
         <Container>
-            <ScrollView>
+            <CustomScrollView>
                 <CarouselHome/>
-                {buttons.map((item, index) => (
-                    <GradientButton
-                        key={index}
-                        text={item.title}
-                        onPress={item.onPress}
-                        style={{marginBottom: 30}}
-                    />
-                ))}
+                <>
+                    {buttons.map((item, index) => (
+                        <GradientButton
+                            key={index}
+                            text={item.title}
+                            onPress={item.onPress}
+                            style={{marginBottom: 30}}
+                        />
+                    ))}
+                </>
                 <DropdownItem
                     title={"Предоставляемые услуги"}
                     show={showServices}
                     setShow={setShowServices}
-                    children={
-                        <></>
-                    }
+                    children={<ServicesInfo/>}
                 />
                 <DropdownItem
                     title={"О Компании"}
                     show={showAbout}
                     setShow={setShowAbout}
-                    children={
-                        <View>
-                            <Text>{aboutCompanyText.text}</Text>
-                        </View>
-                    }
+                    children={<Text>{aboutCompanyText.text}</Text>}
                 />
                 <DropdownItem
                     title={"Новости"}
@@ -73,7 +70,7 @@ const HomeScreen = ({}) => {
                         <></>
                     }
                 />
-            </ScrollView>
+            </CustomScrollView>
         </Container>
     );
 }
