ef87409e31051bc8360100b93e5e7e6a53e6a287
C5345425 <971656316607f16c7e45d3e64a7cfcabf047cc96>
2023-03-19 13:35:09 +0300

    Refactoring

---
+++ src/navigation/CatalogNavigatorScreen.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import {Image, TouchableOpacity, StyleSheet, Platform} from "react-native";
+import {Gradient} from "../components/Gradient";
+import CatalogScreen from "../screens/CatalogScreen";
+import CatalogDetailScreen from "../screens/CatalogDetailScreen";
+import {createStackNavigator} from "@react-navigation/stack";
+import {Color} from "../constants/color";
+
+const Stack = createStackNavigator();
+
+export const CatalogNavigatorScreen = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                options={({navigation}) => ({
+                    headerBackground: () => <Gradient style={styles.flex}/>,
+                    headerLeft: () => (
+                        <TouchableOpacity style={styles.drawerButton} onPress={() => navigation.toggleDrawer()}>
+                            <Image source={require("../../assets/drawer/drawer_white.png")}/>
+                        </TouchableOpacity>
+                    ),
+                    title: "Каталог",
+                    headerTitleStyle: styles.titleStyle
+                })}
+                name="Catalog"
+                component={CatalogScreen}
+            />
+            <Stack.Screen
+                options={({route,navigation}) => ({
+                    headerBackground: () => <Gradient style={styles.flex}/>,
+                    headerLeft: () => (
+                        <TouchableOpacity style={styles.drawerButton} onPress={() => navigation.goBack()}>
+                            <Image source={require("../../assets/drawer/drawer_white.png")}/>
+                        </TouchableOpacity>
+                    ),
+                    //@ts-ignore
+                    title: route.params?.name,
+                    headerTitleStyle: styles.titleStyle
+                })}
+                name="CatalogDetails"
+                component={CatalogDetailScreen}
+            />
+        </Stack.Navigator>
+    );
+}
+
+const styles = StyleSheet.create({
+    drawerButton: {
+        paddingVertical: 12,
+        paddingLeft: 30,
+        paddingRight: 12
+    },
+    titleStyle: {
+        color: Color.white,
+        fontSize: 20,
+        fontWeight: '700'
+    },
+    flex: {flex: 1}
+})
