b4a56736add97755ede4e87f713e09a5fd38482c
C5345425 <971656316607f16c7e45d3e64a7cfcabf047cc96>
2023-03-30 17:14:23 +0300

    Small changes

--- src/components/CustomScrollView.tsx
+++ src/components/CustomScrollView.tsx
@@ -1,10 +1,16 @@
-import {ScrollView,RefreshControl} from "react-native";
+import {ScrollView, RefreshControl} from "react-native";
 import {ScrollViewProps} from "../constants/types";
 
-export const CustomScrollView = ({ children, refresh, refreshing }: ScrollViewProps) => {
+export const CustomScrollView = ({children, refresh, refreshing}: ScrollViewProps) => {
     return (
         <ScrollView
-            refreshControl={<RefreshControl refreshing={refreshing ? refreshing : false} onRefresh={refresh}/>}
+            refreshControl={
+                <RefreshControl
+                    enabled={refreshing}
+                    refreshing={refreshing ? refreshing : false}
+                    onRefresh={refresh}
+                />
+            }
             showsHorizontalScrollIndicator={false}
             showsVerticalScrollIndicator={false}
         >
